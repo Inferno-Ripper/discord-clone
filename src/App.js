f30680763ae51bd513c4e2d0f383e9897cf3fc87
Inferno-Ripper <motasimafridi@icloud.com>
2022-06-02 04:33:56 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+	return (
+		<div className='App'>
+			<Sidebar />
+		</div>
+	);
+}
+
+export default App;
